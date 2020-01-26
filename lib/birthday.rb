74411ac487837864f344278752f758a32435d1a0
Shane Geary <stg4462@comcast.net>
2020-01-26 14:17:30 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,5 +12,8 @@ end
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    if 
+    if age > 12 
+      puts "You are too old for this."
+    end 
+  end 
 end 
